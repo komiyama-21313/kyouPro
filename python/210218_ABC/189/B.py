d531d3f7446074643c932572e89fbae350eb79e6
Keisuke Komiyama <komikei7@gmail.com>
2021-03-16 22:44:49 +0900

    210316_コンテスト2回目

--- python/210218_ABC/189/B.py
+++ python/210218_ABC/189/B.py
@@ -2,11 +2,12 @@ N, X = map(int,input().split())
 
 VP_2dlist = [list(map(int, input().split())) for i in range(N)]
 
+X = X * 100
 alc_tak = 0.0
 count = 0
 for V, P in VP_2dlist:
     count += 1
-    alc = V * P * 0.01
+    alc = V * P
     alc_tak += alc
     if alc_tak > X:
         print(count)
